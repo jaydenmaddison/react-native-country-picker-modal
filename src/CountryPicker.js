03e36f33f8ddf76f24b10dc0a5206d4bbdc1e023
Jayden Maddison <hayden@genflow.com>
2019-08-01 12:53:53 +0100

    text style change

--- src/CountryPicker.js
+++ src/CountryPicker.js
@@ -134,7 +134,7 @@ export default class CountryPicker extends Component {
             : CountryPicker.renderImageFlag(cca2, imageStyle)}
 
         </View>
-        <Text style={{fontSize:16}}>{countryName}</Text>
+        <Text style={{fontSize:16, marginLeft: 0}}>{countryName}</Text>
       </View>
     )
   }
